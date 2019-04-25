3f40dbb96e8046cc772508c97aad7767aefc007c
letnewKid <tevinmcquilkin@gmail.com>
2019-04-24 23:37:17 -0400

    Added functionality

--- popup.js
+++ popup.js
@@ -7,11 +7,12 @@ const input = document.querySelector('input');
 
 // pokeSubmit
 function pokeSubmit(param) {
-  fetch('http://pokeapi.co/api/v2/pokemon/' + param)
+  fetch(`http://pokeapi.co/api/v2/pokemon/${param}`)
     .then(response => response.json())
     .then(result => {
       const imgURL = result.sprites.front_default;
-      const {name} = result;
+      const backURL = result.sprites.back_default;
+      const { name } = result;
       const pokemonNumber = result.id;
 
       // section
@@ -23,36 +24,71 @@ function pokeSubmit(param) {
       pokemonImage.setAttribute('src', imgURL);
       pokemonDisplay.appendChild(pokemonImage);
 
+      // ID
+      const pokemonId = document.createElement('p');
+      pokemonId.innerHTML = `#${pokemonNumber}`;
+      pokemonDisplay.appendChild(pokemonId);
+
       // name
       const pokemonName = document.createElement('p');
       pokemonName.innerHTML = `${name}`;
       pokemonDisplay.appendChild(pokemonName);
 
-      // ID
-      const pokemonId = document.createElement('p');
-      pokemonId.innerHTML = `#${pokemonNumber}`;
-      pokemonDisplay.appendChild(pokemonId);
+      // NEWW
+      // pokemonDisplay.style.border = '1px solid black';
+      pokemonDisplay.style.marginTop = '10px';
+      // pokemonImage.style.border = '1px solid black';
+      // pokemonId.style.border = '1px solid black';
+      // pokemonName.style.border = '1px solid black';
+
+      pokemonDisplay.style.display = 'flex';
+      pokemonId.style.padding = '5px';
+      pokemonName.style.padding = '5px';
+
+      if (backURL !== null) {
+        setInterval(function() {
+          if (pokemonImage.src === imgURL) {
+            pokemonImage.setAttribute('src', backURL);
+          } else {
+            pokemonImage.setAttribute('src', imgURL);
+          }
+        }, 750);
+      }
     })
     .catch(error => {
       console.log(error);
-      const errorMessage = document.createElement('p');
-      errorMessage.innerHTML = 'Not Valid. Try again!';
+      // change to photo
+      const pokemonLogo =
+        'http://pluspng.com/img-png/pokemon-logo-png-pokemon-logo-png-2000.png';
+      const errorMessage = document.createElement('section');
+      const pokemonLogoImg = document.createElement('img');
+      pokemonLogoImg.setAttribute('src', pokemonLogo);
+      pokemonLogoImg.setAttribute('height', '50');
+      errorMessage.appendChild(pokemonLogoImg);
       body.appendChild(errorMessage);
+      errorMessage.style.marginTop = '10px';
     });
 }
 
-// event listeners
+// event listeners (UPDATED)
 button.addEventListener('click', function() {
-  let value = input.value.toLowerCase();
+  const remove = document.querySelector('section');
+  body.removeChild(remove);
+  const value = input.value.toLowerCase();
   pokeSubmit(value);
 });
 
 window.addEventListener('keydown', function(e) {
   if (e.keyCode === 13) {
-    let value = input.value.toLowerCase();
+    const remove = document.querySelector('section');
+    body.removeChild(remove);
+    const value = input.value.toLowerCase();
     pokeSubmit(value);
   }
 });
+
+// pokeSubmit('charmander')
+pokeSubmit('');
 
 // pokeSubmit('charmander');
 // pokeSubmit('pikachu');
